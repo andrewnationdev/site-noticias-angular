847563b84ced67a1b2dba053e1edd53c434d0822
andrewnationdev <21006223@uepg.br>
2026-03-25 22:25:02 -0300

    ✨ Navegação entre páginas

--- src/app/post-summary/post-summary.ts
+++ src/app/post-summary/post-summary.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-post-summary',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './post-summary.html',
   styleUrl: './post-summary.css',
 })
