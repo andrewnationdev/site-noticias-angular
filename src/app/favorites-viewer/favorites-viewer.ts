c68f13409a71374aaf3088b333af89fc790347c6
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-04-12 02:42:56 +0000

    ✨ Adição de toast experimental

--- src/app/favorites-viewer/favorites-viewer.ts
+++ src/app/favorites-viewer/favorites-viewer.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PostSummary } from "../post-summary/post-summary"
+import { Toast } from '../toast/toast'
 import { IPost } from '../../data/types/schema';
+import { IToast } from '../../data/types/toast';
 
 @Component({
   selector: 'app-favorites-viewer',
-  imports: [PostSummary],
+  imports: [PostSummary, Toast],
   standalone: true,
   templateUrl: './favorites-viewer.html',
   styleUrl: './favorites-viewer.css',
@@ -12,6 +14,12 @@ import { IPost } from '../../data/types/schema';
 
 export class FavoritesViewer {
   favorites: IPost[] = [];
+
+  toastProps: IToast = {
+    isVisible: false,
+    text: "This is a message",
+    duration: 5000
+  }
 
   ngOnInit() {
     const storage = localStorage.getItem("favs");
@@ -23,6 +31,12 @@ export class FavoritesViewer {
   deleteFavorite(id: number) {
     this.favorites = this.favorites.filter(post => post.id !== id);
     localStorage.setItem("favs", JSON.stringify(this.favorites));
+
+    this.toastProps = {
+      ...this.toastProps,
+      text: "Deletado dos favoritos!",
+      isVisible: true,
+    };
   }
 
   deleteAllFavorites(){
---
+++ src/data/types/toast.ts
@@ -0,0 +1,5 @@
+export interface IToast {
+    isVisible: boolean;
+    text: string;
+    duration: number;
+}
